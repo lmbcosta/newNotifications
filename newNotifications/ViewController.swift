5175fefffbfa8adb6eee40eddcb7a3ea0447ea43
lmbcosta <lmbcosta@hotmail.com>
2017-09-19 23:33:40 +0100

    Problems with gif image

--- newNotifications/ViewController.swift
+++ newNotifications/ViewController.swift
@@ -7,19 +7,65 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // 1. Request permission
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound], completionHandler: {granted, error in
+            if granted {
+                print("Notification access granted")
+            } else {
+                print(error!.localizedDescription)
+            }
+        })
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func notifyButtonPressed(_ sender: Any) {
+        scheduleNotification(inSeconds: 5, completion: {success in
+            if success {
+                print("Notification created")
+            } else {
+                print("It wasn't possible to create a new notification")
+            }
+        })
     }
-
-
+    
+    func scheduleNotification(inSeconds: TimeInterval, completion: @escaping (_ success: Bool) -> ()) {
+        
+        guard let imageURL = Bundle.main.url(forResource: "rick_grimes", withExtension: "gif") else {
+            completion(false)
+            return
+        }
+        
+        let attachment = try! UNNotificationAttachment(identifier: "myNotification", url: imageURL, options: .none)
+        
+        // Create a notification content
+        let notification = UNMutableNotificationContent()
+        notification.title = "New notification"
+        notification.sound = UNNotificationSound.default()
+        notification.subtitle = "Some text as subtitle"
+        notification.body = "The new notification options are wicked!"
+        notification.attachments = [attachment]
+        
+        // Create  notification Trigger
+        let notificationTrigger = UNTimeIntervalNotificationTrigger(timeInterval: inSeconds, repeats: false)
+        
+        // create notification request
+        let requesNotification = UNNotificationRequest(identifier: "myNotification", content: notification, trigger: notificationTrigger)
+        
+        // Add new notification to qhe center od notifications
+        UNUserNotificationCenter.current().add(requesNotification, withCompletionHandler: {error in
+            if let error = error {
+                print(error.localizedDescription)
+                completion(false)
+            } else {
+                completion(true)
+            }
+        })
+    }
 }
 
